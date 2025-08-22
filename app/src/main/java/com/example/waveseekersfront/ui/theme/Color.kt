904d8d791000bd589b3e4e1f32db35a58ca5bdcc
Leonie Miege <leonie.miege@gmail.com>
2025-08-22 15:38:25 +0200

    update : connecting first 3 activities

--- app/src/main/java/com/example/waveseekersfront/ui/theme/Color.kt
+++ app/src/main/java/com/example/waveseekersfront/ui/theme/Color.kt
@@ -3,9 +3,8 @@ package com.example.waveseekersfront.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Blue40 = Color(0xFF3b576e)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Blue80 = Color(0xFF578099)
+val Orange40 = Color(0xFFcf6929)
+val Grey40 = Color(0xFF777171)
+val Grey80 = Color(0xFFf2f2f2)
 
