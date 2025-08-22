aade59c789130452f44e38661487970bf43724e6
DelphineJozeau <delphine.jozeau@gmail.com>
2025-08-22 16:35:54 +0200

    update : MainActivity Loading

--- app/src/main/java/com/example/waveseekersfront/MainActivity.kt
+++ app/src/main/java/com/example/waveseekersfront/MainActivity.kt
@@ -5,14 +5,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.waveseekersfront.ui.theme.NeueMontrealMediumFontFamily
 import com.example.waveseekersfront.ui.theme.WaveSeekersFrontTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,33 +25,51 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WaveSeekersFrontTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoadingScreen(onLoadingFinished = {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                })
             }
         }
-
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LoadingScreen(onLoadingFinished: () -> Unit) {
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onLoadingFinished()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.wave_seekers_main_logo_light),
+                contentDescription = "Main wave seekers logo",
+                modifier = Modifier.size(180.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = "Loading...",
+                fontFamily = NeueMontrealMediumFontFamily,
+                color = MaterialTheme.colorScheme.primary,
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LoadingPreview() {
     WaveSeekersFrontTheme {
-        Greeting("Android")
+        LoadingScreen(onLoadingFinished = {})
     }
 }
