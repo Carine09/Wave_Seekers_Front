ab1c0934fe38cf9cde908ffae9a7e28ff7150a7d
Caroline Jardy <carolinejardy.pro@gmail.com>
2025-09-10 15:33:55 +0200

    update: renaming AddedSpot.kt to AddedSpotActivity.kt to be the same as others

--- app/src/main/java/com/example/waveseekersfront/AddedSpotsActivity.kt
+++ app/src/main/java/com/example/waveseekersfront/AddedSpotsActivity.kt
@@ -17,15 +17,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
